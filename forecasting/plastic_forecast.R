6edc91c2c4ceea82e538c5f9125b08bf4e6affde
Tocheee <tochi.okeke@gmail.com>
2020-09-12 17:36:32 -0700

    added in basic plot of fitted values

--- forecasting/plastic_forecast.R
+++ forecasting/plastic_forecast.R
@@ -14,42 +14,113 @@ dat %<>%
          !(Year %in% 2016 & MonthNum < 6))
 
 #Task is to forecast the number of plastics over time
-#First visualize the current number of plastics over time
+summary(dat)
+
+dat$DateStandardized <- lubridate::ymd_hms(dat$DateStandardized)
 
 #Select only columns that count the number of plastics
 plastic_dat <- dat %>% 
   dplyr::select(Location,
                 TotalVolunteers,
+                DateStandardized,
                 Month,
                 Year,
                 Totalltems_EventRecord:FishingOtherPlasticDebris2) %>% 
   dplyr::select(-c(COUNTRY:CONTINENT)) %>% 
   mutate(totalplastic = PCT_PlasticAndFoam*Totalltems_EventRecord) %>% 
   group_by(Month,Year) %>% 
-  mutate(totalplastic = sum(totalplastic,na.omit)) %>% 
+  mutate(totalplastic = sum(totalplastic,na.rm=T)) %>% 
+  dplyr::select(-Count_,
+                -FishingOtherPlasticDebris2,
+                -FishingGlowSticks2,
+                -Soft_Sheets2,
+                -SUM_HardSoftLollipopStick_EarBu,
+                -SUM_HardOrSoft_PlasticBottleCap) %>% 
   na.omit() %>% 
   unique()
 
+# #Train
+# #APPROACH 1
+# model_plastic.lm <- lm(totalplastic~.,
+#                data=plastic_dat,
+#                na.action=na.fail)
+# 
+# summary(model_plastic.lm)
+# varImp(model_plastic.lm)
+# # plot(model.lm)
+# 
+# #Approach 2
+# all_vars_model_plastic <- lm(totalplastic~.,
+#                      data=plastic_dat,
+#                      na.action = na.fail)
+# #Use stepwise multiple regression to determine the best model
+# step_model_eval_plastic <- stepAIC(all_vars_model_plastic, direction= "both")
+# step_model_eval_plastic$anova
+# #summarize the model determined by the stepAIC function to be the best fit
+# summary(step_model_eval_plastic)
+# varImp(step_model_eval_plastic)
+# print(step_model_eval_plastic$call)
+# 
+# 
+# 
+# 
 
-#Split data with 80% in the training set
 
-train_plastic <- plastic_dat[1:2400,] %>% 
-  na.omit
+#Approach 3
+#Based on correlations and feature exploration
+# calculate correlation matrix
+library(DataExplorer)
+#Discrete correlation
+cor_discrete <- plot_correlation(na.omit(plastic_dat),type="d")
+print(cor_discrete)
+#Continuos correlation
+cor_cts <- plot_correlation(na.omit(plastic_dat),type="c")
+print(cor_cts)
 
-test_plastic <- plastic_dat[2401:3012,] %>% 
-  na.omit
 
-#Train
-#APPROACH 1
-model.lm <- lm(totalplastic~`Location`+
-                 `Month`+
-                 `Year`+
-                 `TotalVolunteers`,
-               data=train_plastic,
-               na.action=na.fail)
-summary(model.lm)
-plot(model.lm)
+model_3 <- lm(totalplastic~
+                `TotalVolunteers` +
+                LAND_TYPE +
+                LAND_RANK +
+                Month + 
+                Year +
+                Location+
+                DateStandardized,
+              data=plastic_dat, na.action = na.fail)
 
-#Forecast
-forecast(model.lm,test_plastic)
+model_3_plastic_data <- plastic_dat %>% 
+  dplyr::select(DateStandardized,
+                totalplastic
+                ,TotalVolunteers,
+                LAND_TYPE,
+                LAND_RANK,
+                Month,
+                Year,
+                Location)
 
+model_3_plastic_data <- unique(model_3_plastic_data)
+
+model_3_plastic <- lm(totalplastic~.,
+                    data=model_3_plastic_data, na.action = na.fail)
+
+summary(model_3_plastic)
+varImp(model_3_plastic)
+
+
+#Use model 3 and plot the fitted values for 2018
+plot_df <- data.frame(DateVar = model_3_plastic_data$DateStandardized,
+                      Month=model_3_plastic_data$Month, 
+                      Year=model_3_plastic_data$Year,
+                      Fitted=model_3_plastic$fitted.values,
+                      Actual=model_3_plastic_data$totalplastic)
+
+#Plot
+library(plotly)
+plot_plastic <- plot_df %>% 
+  ggplot(aes(DateVar,Fitted)) + 
+  geom_line()
+
+interacticve_plastic <- ggplotly(plot_plastic)
+
+interacticve_plastic
+#Linear Extrapolation
--- forecasting/waste_forecast.R
+++ forecasting/waste_forecast.R
@@ -5,8 +5,10 @@ library(caret)
 library(ggplot2)
 library(arrow)
 library(forecast)
+library(MASS)
+library(lubridate)
 
-dat <- read_parquet("raw_data/Combined_MLW_MDMAP_TIDES_Data__All_Points_2015-2018.parquet")
+dat <- read_csv("raw_data/_Combined_MLW,_MDMAP,_TIDES_Data__All_Points,_2015-2018.csv")
 
 #Filter for Canadian data
 dat %<>% 
@@ -14,45 +16,93 @@ dat %<>%
          !Year %in% c(2015),
          !(Year %in% 2016 & MonthNum < 6))
 
+#Change Location to Factor
+dat$Location = as.factor(dat$Location)
+
+dat$DateStandardized <- lubridate::ymd_hms(dat$DateStandardized)
 
 #Task is to forecast the number of total waste over time
 #First visualize the current number of waste over time
 #Group by month year and reduce dataset
 month_year_dat <- dat %>% 
   dplyr::select(Location,
+         DateStandardized,
          TotalVolunteers,
          Month,
          Year,
          Totalltems_EventRecord:FishingOtherPlasticDebris2) %>% 
   dplyr::select(-c(COUNTRY:CONTINENT)) %>% 
   group_by(Month,Year) %>% 
-  mutate(Totalltems_EventRecord = as.numeric(sum(Totalltems_EventRecord)),) %>% 
+  mutate(Totalltems_EventRecord = as.numeric(sum(Totalltems_EventRecord))) %>%
+  dplyr::select(-Count_,
+                -FishingOtherPlasticDebris2,
+                -FishingGlowSticks2,
+                -Soft_Sheets2,
+                -SUM_HardSoftLollipopStick_EarBu,
+                -SUM_HardOrSoft_PlasticBottleCap) %>% 
   na.omit() %>% 
   unique()
 
-#Split data with 80% in the training set
-
-train_waste <- month_year_dat[1:2400,] %>% 
-  na.omit()
-
-test_waste <- month_year_dat[2401:3012,] %>% 
-  na.omit()
-
-#Select certain variables
 
 
 #Train
-#APPROACH 1
-model.lm <- lm(Totalltems_EventRecord~`Location`+
-                    `Month`+
-                   `Year`+
-                  `TotalVolunteers`,
-                  data=train_waste,
-                  na.action=na.fail)
-summary(model.lm)
-# plot(model.lm)
+# #APPROACH 1
+# model.lm <- lm(Totalltems_EventRecord~`Location`+
+#                     `Month`+
+#                    `Year`+
+#                   `TotalVolunteers`,
+#                   data=month_year_dat,
+#                   na.action=na.fail)
+# summary(model.lm)
+# # plot(model.lm)
+# 
+# #Approach 2
+# all_vars_model_waste <- lm(Totalltems_EventRecord~.,
+#                      data=month_year_dat,
+#                      na.action = na.fail)
+# #Use stepwise multiple regression to determine the best model
+# step_model_eval_waste <- stepAIC(all_vars_model_waste, direction= "both")
+# step_model_eval_waste$anova
+# #summarize the model determined by the stepAIC function to be the best fit
+# summary(step_model_eval_waste)
+# print(step_model_eval_waste$call)
 
-#Forecast
-predict.linear <- predict(model.lm,test_waste)
+#Approach 3
+library(DataExplorer)
+#Discrete correlation
+cor_discrete <- plot_correlation(na.omit(month_year_dat),type="d")
+print(cor_discrete)
+#Continuos correlation
+cor_cts <- plot_correlation(na.omit(month_year_dat),type="c")
+print(cor_cts)
 
-summary(predict.lm)
+model_3_waste_data <- month_year_dat %>% 
+  dplyr::select(DateStandardized,
+    Totalltems_EventRecord
+                ,TotalVolunteers,
+                LAND_TYPE,
+                LAND_RANK,
+                Month,
+                Year,
+                Location)
+
+model_3_waste_data <- unique(model_3_waste_data)
+
+model_3_waste <- lm(Totalltems_EventRecord~.,
+              data=model_3_waste_data, na.action = na.fail)
+
+summary(model_3_waste)
+varImp(model_3_waste)
+
+
+#Use model 3 and plot the fitted values
+waste_df <- data.frame(Date_Var = model_3_waste_data$DateStandardized,
+                      Fitted=model_3_waste$fitted.values,
+                      Actual=model_3_waste_data$Totalltems_EventRecord,
+                      Location=model_3_waste_data$Location)
+
+
+#Plot
+plot_waste <- waste_df %>% ggplot(aes(Date_Var,Fitted)) + geom_line()
+
+ggplotly(plot_waste)
